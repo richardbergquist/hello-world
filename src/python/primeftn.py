66d6e51e8fbfb99540495ff8479ad2f52bc29d43
richardbergquist <bergquistr@gmail.com>
2016-09-16 14:33:03 +1200

    updates

--- src/python/primeftn.py
+++ src/python/primeftn.py
@@ -13,3 +13,17 @@ def calcprimes(limit):
             primes.append(n)
 
     return primes
+
+def printprimes(limit):
+    """Print prime numbers up to n."""
+    
+    for n in range(2, limit):
+        for x in range(2, n):
+            if n % x == 0:
+                # print n, 'equals', x, '*', n/x
+                break
+        else:
+            # loop fell through without finding a factor
+            print (n, end=", ", flush=True)
+
+    print("")
