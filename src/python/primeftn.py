02c1da8d4317f89870e4fba19badd5da19401a8c
richardbergquist <bergquistr@gmail.com>
2016-09-16 15:40:36 +1200

    updates.

--- src/python/primeftn.py
+++ src/python/primeftn.py
@@ -14,10 +14,40 @@ def calcprimes(limit):
 
     return primes
 
+def calcprimes(start, limit):
+    """Calc prime numbers between start and limit."""
+    primes = []
+
+    for n in range(start, limit):
+        for x in range(2, n):
+            if n % x == 0:
+                # print n, 'equals', x, '*', n/x
+                break
+        else:
+            # loop fell through without finding a factor
+            # print n,
+            primes.append(n)
+
+    return primes
+
 def printprimes(limit):
     """Print prime numbers up to n."""
-    
+
     for n in range(2, limit):
+        for x in range(2, n):
+            if n % x == 0:
+                # print n, 'equals', x, '*', n/x
+                break
+        else:
+            # loop fell through without finding a factor
+            print (n, end=", ", flush=True)
+
+    print("")
+
+def printprimes(start, limit):
+    """Print prime numbers between start and limit."""
+
+    for n in range(start, limit):
         for x in range(2, n):
             if n % x == 0:
                 # print n, 'equals', x, '*', n/x
